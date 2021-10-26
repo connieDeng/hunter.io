cf8fe2486a35fdf9fe87a7d193e3ff1a0e24cfda
connieDeng <dengconnie0723@gmail.com>
2021-10-26 19:41:16 -0400

    updating changes to everything

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { LandingPage } from './pages/landing-page';
+// import { LandingPage } from './pages/landing-page';
 import "./App.css";
 import { SuccessLogin } from './pages/success-login';
-import { Game } from './pages/game';
+// import { Game } from './pages/game';
 // import gameCanvas from './pages/gameCanvas';
-import { ProtectedRoute } from './components/protected-route';
+// import { ProtectedRoute } from './components/protected-route';
+import socket from "./socket";
+
+socket.on("connect", () => {
+  console.log("app connected:", socket.id)
+});
 
 function App() {
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/hunter.io" component={SuccessLogin} />
-        <Route exact path="/game" component={Game} />
+        <Route exact path="/" component={SuccessLogin} />
+        {/* <Route exact path="/hunter.io" component={SuccessLogin} /> */}
+        {/* <Route exact path="/game" component={Game} /> */}
         <Route path="*" component={() => "404 NOT FOUND"} />
       </Switch>
     </BrowserRouter>
--- client/src/pages/success-login.js
+++ client/src/pages/success-login.js
@@ -1,12 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import auth from './../components/auth';
+// import changeDirection from './../../../backend/game';
+import socket from "../socket";
 
 export const SuccessLogin = (props) => {
+    const [board, setBoard] = useState(null);
+
+    const startGame = () => { 
+        socket.emit("initPlayer");
+        
+        socket.on("initializedPlayer", (board) => {
+            setBoard(board);
+        }); 
+        // socket.emit("moveSnake", socket.id);
+    };
+    
+    socket.on("playerMoved", (board) => { 
+        console.log("player moved")
+    });
+
+    socket.on("updatedStateBoard", (board) => { 
+        setBoard(board);
+    });
+
+    const moveSnake = () => {
+        socket.emit("moveSnake");
+    };
     
     return(
-        <div>
-            aii yo got in
-        </div> 
+        <div className='container'>
+            <div> {"honestly please why"} </div>
+            
+            <div> 
+            { board !== null ? 
+                <div>
+                    {board.map((row, i) => (
+                        <div key={i}>
+                        {row.map((col, j) => (
+                            <span key={j}>{col}</span>
+                        ))}
+                        </div>
+                    ))} 
+                </div> :
+                <div> NO BOARD </div>
+            }
+            </div>
+            
+            <button onClick={}>CHANGE DIR</button>
+            <button onClick={moveSnake}>MOVE</button>
+            <button onClick={startGame}>Start Game</button>
+        </div>
     );
     
 }
